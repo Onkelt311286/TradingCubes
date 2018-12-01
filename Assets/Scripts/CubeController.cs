d2c785697383b604d798941cc6d3e8bcd4929718
onkelt311286 <onkelt311286@googlemail.com>
2018-12-01 20:14:52 +0100

    initialer commit

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeController : MonoBehaviour{
+
+    public int population;
+    private Text populatiopnText;
+
+    private GameController gameController;
+
+    private void Start()
+    {
+        populatiopnText = this.gameObject.GetComponentInChildren<Text>();
+        populatiopnText.text = population.ToString();
+
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+            gameController = gameControllerObject.GetComponent<GameController>();
+        gameController.addSelectionController(this);
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        Debug.Log("Selected: " + gameObject.name);
+        calculatePopulation();
+    }
+
+    public void calculatePopulation()
+    {
+        population--;
+        populatiopnText.text = population.ToString();
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    //Make sure to attach these Buttons in the Inspector
+    public Button m_YourFirstButton, m_YourSecondButton, m_YourThirdButton, nextRoundButton;
+    private int roundCounter = 0;
+    public Text roundCounterText;
+
+    private List<CubeController> selectionControllers = new List<CubeController>();
+
+    private void Start()
+    {
+        //Calls the TaskOnClick/TaskWithParameters/ButtonClicked method when you click the Button
+        m_YourFirstButton.onClick.AddListener(TaskOnClick);
+        m_YourSecondButton.onClick.AddListener(delegate { TaskWithParameters("Hello"); });
+        m_YourThirdButton.onClick.AddListener(() => ButtonClicked(42));
+        m_YourThirdButton.onClick.AddListener(TaskOnClick);
+
+        nextRoundButton.onClick.AddListener(nextRound);
+    }
+
+    private void nextRound()
+    {
+        roundCounter++;
+        roundCounterText.text = "Runde: " + roundCounter.ToString();
+        foreach (CubeController selectionController in selectionControllers)
+        {
+            selectionController.calculatePopulation();
+        }
+    }
+
+    public void addSelectionController(CubeController selectionController)
+    {
+        selectionControllers.Add(selectionController);
+    }
+
+    void TaskOnClick()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button!");
+    }
+
+    void TaskWithParameters(string message)
+    {
+        //Output this to console when the Button2 is clicked
+        Debug.Log(message);
+    }
+
+    void ButtonClicked(int buttonNo)
+    {
+        //Output this to console when the Button3 is clicked
+        Debug.Log("Button clicked = " + buttonNo);
+    }
+}
